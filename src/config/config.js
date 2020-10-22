fa895df59d1ffbd67854a000c3db10c9d94c350e
abc <abc@example.com>
2020-10-21 21:17:22 -0300

    adicionando variaveis

--- src/config/config.js
+++ src/config/config.js
@@ -1,37 +1,25 @@
-// Define a string de conexão com o banco de dados 
-
-module.exports = { 
-
-    development: { 
-
-        database: { 
-
-            host: 'localhost', 
-
-            port: 3305, 
-
-            name: 'db_usuario', 
-
-            dialect: 'mysql', 
-
-            user: 'root', 
-
-            password: 'adriana1993@' 
-
-        } 
-
-    }, 
-
-    production:{ 
-
-        database: { 
-
-            host: process.env.DB_HOST, 
-
-            host: process.env.DB_PORT 
-
-        } 
-
-    } 
-
-} 
+require('dotenv/config');
+ 
+// Define a string de conexão com o banco de dados
+module.exports = {
+    development: {
+        database: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        }
+    },
+    production: {
+        database: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: process.env.DB_DIALECT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        }
+    }
+}
